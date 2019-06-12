bc63f3f79642a76400944b9d44b4c639bdcbadeb
J_Aydin <janberd.aydin@gmail.com>
2019-06-12 14:32:08 +0200

    pushed

--- include/header.php
+++ include/header.php
@@ -11,7 +11,7 @@
                   </div>
                   <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-xs-12">
                     <div class="r-header-action float-right">
-                      <a href="login-register.php"> <img src="assets/images/icon_login.png" alt=""> <span>Login</span></a>
+                      <a href="login-register.php" data-toggle="modal" data-target="#loginModal" > <img src="assets/images/icon_login.png" alt=""> <span>Login</span></a>
                       <a href="#" class="r-search"> <img src="assets/images/icon_search.png" alt=""> <span>Search</span></a>
 
                       <div class="r-search-wrapper">
@@ -25,6 +25,27 @@
                         </div>
                       </div> <!-- /r-search-wrapper -->
                     </div>
+
+                      <!-- Modal -->
+                      <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered" role="document">
+                              <div class="modal-content">
+                                  <div class="modal-header">
+                                      <h5 class="modal-title" id="exampleModalLongTitle">Inloggen</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                      </button>
+                                  </div>
+                                  <div class="modal-body">
+                                      
+                                  </div>
+                                  <div class="modal-footer">
+                                      <button type="button" class="btn btn-primary">Login</button>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+
                     <div class="r-nav-section float-right">
                       <nav>
                         <ul>
